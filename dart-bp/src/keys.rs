4748f951daf5cef0f1d70d9fbbebf489dfb82878
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-06-13 02:47:29 +0800

    Derive some standard traits.

--- dart-bp/src/keys.rs
+++ dart-bp/src/keys.rs
@@ -2,9 +2,16 @@ use ark_ec::{AffineRepr, CurveGroup};
 use ark_std::UniformRand;
 use rand::RngCore;
 
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct VerKey<PK: AffineRepr>(pub PK);
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct SigKey<PK: AffineRepr>(pub PK::ScalarField);
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct EncKey<PK: AffineRepr>(pub PK);
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub struct DecKey<PK: AffineRepr>(pub PK::ScalarField);
 
 pub fn keygen_sig<R: RngCore, PK: AffineRepr>(rng: &mut R, g: PK) -> (SigKey<PK>, VerKey<PK>) {
