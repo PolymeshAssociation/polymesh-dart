4188c4d95565aa2965f1bc3b740b464f700a2493
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2026-03-22 05:16:43 +0800

    More no statics.

--- src/bp/account.rs
+++ src/bp/account.rs
@@ -566,7 +566,7 @@ impl AccountAssetRegistrationProof {
     ) -> Result<(Self, AccountAssetState), Error> {
         let account_state = keys.init_asset_state(asset_id, counter, identity)?;
         let (bp_state, commitment) = account_state.bp_current_state(keys)?;
-        let params = poseidon_params();
+        let params = PoseidonParameters::new()?;
         let gens = dart_gens();
         let proof = account_registration::RegTxnProof::new(
             rng,
