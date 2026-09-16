78c53c861c2896f537bfc61bd75a048031154b6d
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2026-09-17 02:32:28 +0800

    Add is_party method.

--- src/bp/leg.rs
+++ src/bp/leg.rs
@@ -7,7 +7,8 @@ use serde::{Deserialize, Serialize};
 use codec::{Decode, DecodeWithMemTracking, Encode, MaxEncodedLen};
 use scale_info::TypeInfo;
 
-use ark_ec::{CurveConfig, short_weierstrass::Affine};
+use ark_ec::{AffineRepr, CurveConfig, CurveGroup, short_weierstrass::Affine};
+use ark_ff::Field;
 use ark_std::{
     collections::BTreeSet,
     format,
@@ -1502,6 +1503,101 @@ impl LegEncrypted {
         } else {
             None
         }
+    }
+
+    /// Check if the given key and account correspond to a party in this leg.
+    ///
+    /// Investors should:
+    /// - Provide their account public key in the `account` parameter.
+    /// - `max_asset_id` and `auditor_check` should be `None`.
+    ///
+    /// Mediators and auditors should:
+    /// - Provide `None` for the `account` parameter.
+    /// - Provide the appropriate `max_asset_id` and `auditor_check` values.
+    ///
+    /// The `auditor_check` closure is used to filter asset IDs the auditor isn't interested in.  The closure should return `Some(balance)` if the auditor wants to check the asset ID, or `None` otherwise.
+    /// If possible the auditor check closure should return the asset's total supply as the `Balance` value for faster decryption.
+    pub fn is_party<F: Fn(&AssetId) -> Option<Balance>>(
+        &self,
+        key: &EncryptionKeyPair,
+        account: Option<AccountPublicKey>,
+        max_asset_id: Option<AssetId>,
+        auditor_check: Option<F>,
+    ) -> Result<Option<LegRole>, Error> {
+        use polymesh_dart_bp::leg::LegEncryption;
+
+        let leg_enc = self.decode()?;
+
+        let sk_enc_inv = key
+            .secret
+            .0
+            .0
+            .inverse()
+            .ok_or_else(|| Error::LegDecryptionError("Inverse failed".into()))?;
+
+        // If we have the account public key, we can check if this key corresponds to the sender or receiver in the leg.
+        if let Some(account) = account {
+            let account = account.get_affine()?;
+            // Try to decrypt as the sender.
+            {
+                let sender = LegEncryption::decrypt_element_with_sk_inv(
+                    &sk_enc_inv,
+                    leg_enc.leg_enc_core_and_eph_keys.core.ct_s,
+                    leg_enc.leg_enc_core_and_eph_keys.eph_pk_s.r1,
+                )
+                .into_affine();
+                if sender == account {
+                    return Ok(Some(LegRole::sender()));
+                }
+            }
+            // Try to decrypt as the receiver.
+            {
+                let receiver = LegEncryption::decrypt_element_with_sk_inv(
+                    &sk_enc_inv,
+                    leg_enc.leg_enc_core_and_eph_keys.core.ct_r,
+                    leg_enc.leg_enc_core_and_eph_keys.eph_pk_r.r2,
+                )
+                .into_affine();
+                if receiver == account {
+                    return Ok(Some(LegRole::receiver()));
+                }
+            }
+        }
+
+        if let Some(auditor_check) = auditor_check {
+            let enc_gen = dart_gens().leg_asset_value_gen().into_group();
+
+            let num_enc_keys = leg_enc.eph_pk_enc_keys.len();
+
+            if let Some(asset_id) = leg_enc.asset_id() {
+                // The asset ID is revealed, so we have to try decrypting the leg `amount`.
+                if let Some(balance) = auditor_check(&asset_id) {
+                    for key_idx in 0..num_enc_keys {
+                        if leg_enc
+                            .decrypt_amount(&sk_enc_inv, false, key_idx, enc_gen, balance)
+                            .is_ok()
+                        {
+                            return Ok(Some(LegRole::auditor(key_idx as u8)));
+                        }
+                    }
+                } else {
+                    // The auditor is not interested in this asset ID.
+                    return Ok(None);
+                }
+            } else {
+                let max_asset_id = max_asset_id.unwrap_or(MAX_ASSET_ID);
+                // The asset ID is not revealed, so we need to try decrypting the asset ID.
+                for key_idx in 0..num_enc_keys {
+                    if leg_enc
+                        .decrypt_asset_id(&sk_enc_inv, false, key_idx, enc_gen, max_asset_id)
+                        .is_ok()
+                    {
+                        return Ok(Some(LegRole::auditor(key_idx as u8)));
+                    }
+                }
+            }
+        }
+        Ok(None)
     }
 
     /// Decrypt the leg using the provided role and account keys.  Only use this if the role is known.
